706e3c34fe4838112f51e96dee93a56aa7c7e0a6
bakatrouble <bakatrouble@gmail.com>
2025-11-28 13:58:40 +0300

    fix get_tweet_by_id

--- twikit/client/client.py
+++ twikit/client/client.py
@@ -1523,7 +1523,7 @@ class Client:
                 results.append(tweet)
 
         if entries[-1]['entryId'].startswith('cursor'):
-            next_cursor = entries[-1]['content']['itemContent']['value']
+            next_cursor = entries[-1]['content']['value']
             _fetch_next_result = partial(self._get_more_replies, tweet_id, next_cursor)
         else:
             next_cursor = None
@@ -1632,7 +1632,7 @@ class Client:
 
         if entries[-1]['entryId'].startswith('cursor'):
             # if has more replies
-            reply_next_cursor = entries[-1]['content']['itemContent']['value']
+            reply_next_cursor = entries[-1]['content']['value']
             _fetch_more_replies = partial(self._get_more_replies,
                                           tweet_id, reply_next_cursor)
         else:
